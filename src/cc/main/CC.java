0790a351a0e86ac52035a5398a8bcbbbf1a54008
bleeben <bleeben@mit.edu>
2012-03-24 23:27:21 -0400

    fixing bugs

---
+++ src/cc/main/CC.java
@@ -0,0 +1,13 @@
+package cc.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CC extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
