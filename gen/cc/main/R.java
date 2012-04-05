a538c0f647c8abc1fb1555d1a03c451797cab50f
bleeben <bleeben@mit.edu>
2012-04-05 17:48:57 -0400

    sort of got home and collections layout ui xml

--- gen/cc/main/R.java
+++ gen/cc/main/R.java
@@ -13,11 +13,62 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int btnBrowseColl=0x7f050007;
+        public static final int btnNewColl=0x7f050006;
+        public static final int btnNewPic=0x7f050005;
+        public static final int btnRecent=0x7f050008;
+        public static final int button1=0x7f050003;
+        public static final int editText1=0x7f050004;
+        public static final int gridView1=0x7f050001;
+        public static final int textView1=0x7f050000;
+        public static final int textView2=0x7f050002;
+        public static final int textview=0x7f050009;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int collections=0x7f030000;
+        public static final int home=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
+        public static final int addToCollection=0x7f040006;
+        public static final int advBarcode=0x7f04001d;
+        public static final int advCamera=0x7f04001e;
+        public static final int advSearch=0x7f04001b;
+        public static final int advSummary=0x7f04001a;
+        public static final int advTag=0x7f04001c;
         public static final int app_name=0x7f040001;
+        public static final int browseCollections=0x7f040004;
+        public static final int btnAdd=0x7f040025;
+        public static final int btnCancel=0x7f040021;
+        public static final int btnDone=0x7f04001f;
+        public static final int btnOk=0x7f040022;
+        public static final int btnReturn=0x7f040023;
+        public static final int btnSave=0x7f040020;
+        public static final int btnShare=0x7f040024;
+        public static final int collectionProperties=0x7f040008;
+        public static final int collections=0x7f040002;
+        public static final int fieldDesc=0x7f04000d;
+        public static final int fieldName=0x7f04000c;
+        public static final int fieldThumbnail=0x7f04000e;
+        public static final int filter=0x7f040015;
         public static final int hello=0x7f040000;
+        public static final int item=0x7f040003;
+        public static final int menu=0x7f040017;
+        public static final int menuProperties=0x7f040019;
+        public static final int menuView=0x7f040018;
+        public static final int newCollection=0x7f040009;
+        public static final int newContact=0x7f040014;
+        public static final int newItem=0x7f04000b;
+        public static final int newPic=0x7f04000a;
+        public static final int permEdit=0x7f040012;
+        public static final int permOwn=0x7f040013;
+        public static final int permView=0x7f040011;
+        public static final int recentItems=0x7f040005;
+        public static final int sharePrivate=0x7f040010;
+        public static final int sharePublic=0x7f04000f;
+        public static final int sharingManager=0x7f040007;
+        public static final int tags=0x7f040016;
+        public static final int untitled=0x7f040026;
     }
 }
