039930319d5c7ca70e908c19ca84fdff970fcb3f
bleeben <bleeben@mit.edu>
2012-04-13 18:12:30 -0400

    functional buttons for the first time

---
+++ src/cc/main/CCActivity.java
@@ -0,0 +1,26 @@
+package cc.main;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class CCActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+    //hi
+    
+    public void onBrowseCollectionsButtonClick(View view) {
+    	Intent intent = new Intent(this, CollectionsActivity.class);
+    	startActivity(intent);
+    }
+    
+    public void onNewCollectionsButtonClick(View view) {
+    	Intent intent = new Intent(this, CollectionPropertiesActivity.class);
+    	startActivity(intent);
+    }
+}
