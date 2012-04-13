7eb36e6e5cb19c1dd1f6efd4bd34d9ba1552bb7d
bleeben <bleeben@mit.edu>
2012-04-13 18:20:48 -0400

    added some xmls and javas, but also testing email system

--- gen/cc/main/R.java
+++ gen/cc/main/R.java
@@ -41,6 +41,7 @@ public final class R {
         public static final int collections=0x7f030001;
         public static final int home=0x7f030002;
         public static final int main=0x7f030003;
+        public static final int new_item=0x7f030004;
     }
     public static final class string {
         public static final int addToCollection=0x7f040006;
