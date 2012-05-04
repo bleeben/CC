65d8c4341f57a3c7c3e33e362d17a7dd2995e000
bleeben <bleeben@mit.edu>
2012-05-04 01:37:03 -0400

    arg just adding more files

---
+++ src/data/hash/FKSPerfectHash.java
@@ -0,0 +1,35 @@
+package data.hash;
+
+public class FKSPerfectHash<K,V> extends BasicHashMap<K, V>{
+
+	@Override
+	public V put(K key, V value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V get(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V remove(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean containsKey(K key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(V value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
