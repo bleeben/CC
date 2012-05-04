a77573cbb306319d2f8a4d61fd2c788de93ddc4f
bleeben <bleeben@mit.edu>
2012-05-03 23:44:56 -0400

    abstract class to deal with some stuff of general hash capacities

--- src/data/hash/BasicHashMap.java
+++ src/data/hash/BasicHashMap.java
@@ -5,4 +5,8 @@ public abstract class BasicHashMap<K, V> implements HashMapStructure<K, V> {
 	protected int size;
 	protected K[] keys;
 	protected V[] values;
+	protected float minLoad; // 1/3
+	protected float maxLoad; // 2/3
+	protected float prefLoad; // 1/2?
+	// a good load will be between 1/3 and 2/3? subject to change
 }
