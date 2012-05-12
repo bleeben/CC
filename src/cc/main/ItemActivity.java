027a4b8174758c9fc090b4d92abaf7f5b3ab4440
bleeben <bleeben@mit.edu>
2012-05-12 17:50:04 -0400

    working on itemactivity stuff, dont touch them, volatile

--- src/cc/main/ItemActivity.java
+++ src/cc/main/ItemActivity.java
@@ -1,11 +1,16 @@
 package cc.main;
 
+import java.util.ArrayList;
+
 import cc.rep.Collection;
 import cc.rep.Item;
+import cc.rep.ResultCode;
+import cc.rep.SpinnerListAdapter;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,6 +18,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +28,8 @@ public class ItemActivity extends Activity {
 	Item item;
 	int position;
 	TextView itemName;
+    Spinner collectionSpinner;
+
 	
     /** Called when the activity is first created. */
     @Override
@@ -31,6 +40,19 @@ public class ItemActivity extends Activity {
         Intent i = getIntent();
         item = (Item) i.getParcelableExtra("item");
         position = i.getIntExtra("position", 0);
+        
+        itemName = (TextView)findViewById(R.id.textViewItemName);
+        itemName.setText(item.getName());
+        
+        collectionSpinner = (Spinner) findViewById(R.id.collectionSpinner);
+        
+        Collection c = (Collection) i.getParcelableExtra("collection");
+        ArrayList<Collection> cs = i.getParcelableArrayListExtra("collections");
+        if (cs==null) {
+        	collectionSpinner.setAdapter(new SpinnerListAdapter(this,getLayoutInflater(),c));
+        } else {
+        	collectionSpinner.setAdapter(new SpinnerListAdapter(this,getLayoutInflater(),cs));
+        }
         
     }
     //hi
@@ -44,6 +66,7 @@ public class ItemActivity extends Activity {
     	item.setName(itemName.getText().toString());
     	Intent intent = new Intent();
     	intent.putExtra("item", item);
+    	intent.putExtra("position", position);
     	setResult(Activity.RESULT_OK, intent);
     	finish();
     }
@@ -81,6 +104,19 @@ public class ItemActivity extends Activity {
     	intent.putExtra("item", item);
     	intent.putExtra("position", position);
     	setResult(Activity.RESULT_OK, intent);
+    }
+    
+    public void takePicture(View view) {
+    	Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+    	startActivityForResult(cameraIntent, ResultCode.CAMERA_PIC_REQUEST);
+    }
+    
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    	if (requestCode == ResultCode.CAMERA_PIC_REQUEST){
+    		Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+    		ImageView image = (ImageView) findViewById(R.id.imageView1);
+    		image.setImageBitmap(thumbnail);
+    	}
     }
     
     @Override
