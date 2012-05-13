1dda92b6d7667bdedd2d3fa377c8985c88b41624
Thomas Schultz <tschultz@mit.edu>
2012-05-13 12:44:56 -0400

    I think i chagned something

--- src/data/hash/SkeletonHashMap.java
+++ src/data/hash/SkeletonHashMap.java
@@ -38,12 +38,13 @@ public abstract class SkeletonHashMap<K, V> {
 	 * for hashCodes that do not differ in lower bits. Note: Null keys always
 	 * map to hash 0, thus index 0.
 	 */
-	static int hash(int h) {
+	public static int hash(int h) {
 		// This function ensures that hashCodes that differ only by
 		// constant multiples at each bit position have a bounded
 		// number of collisions (approximately 8 at default load factor).
 		h ^= (h >>> 20) ^ (h >>> 12);
-		return h ^ (h >>> 7) ^ (h >>> 4);
+		return h ^ (h >>> 7) ^ (h >>> 4) ;
+		
 	}
 	
 	public int nearestPowerOfTwo(int capacity){
